09fb5e3124e3a51831d4b6fed898ed728ea2f83f
uatrade <piskovtsev@gmail.com>
2018-08-29 21:35:26 +0300

    Add project files.

---
+++ LibraryWinFormEntity/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace LibraryWinFormEntity
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbFind = new System.Windows.Forms.TextBox();
+            this.btFind = new System.Windows.Forms.Button();
+            this.dataGridViewAuthor = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewAuthor)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tbFind
+            // 
+            this.tbFind.Location = new System.Drawing.Point(103, 46);
+            this.tbFind.Name = "tbFind";
+            this.tbFind.Size = new System.Drawing.Size(266, 20);
+            this.tbFind.TabIndex = 0;
+            this.tbFind.TextChanged += new System.EventHandler(this.tbFind_TextChanged);
+            // 
+            // btFind
+            // 
+            this.btFind.Location = new System.Drawing.Point(103, 175);
+            this.btFind.Name = "btFind";
+            this.btFind.Size = new System.Drawing.Size(253, 23);
+            this.btFind.TabIndex = 1;
+            this.btFind.Text = "Find";
+            this.btFind.UseVisualStyleBackColor = true;
+            this.btFind.Click += new System.EventHandler(this.btFind_Click);
+            // 
+            // dataGridViewAuthor
+            // 
+            this.dataGridViewAuthor.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewAuthor.Location = new System.Drawing.Point(388, 46);
+            this.dataGridViewAuthor.Name = "dataGridViewAuthor";
+            this.dataGridViewAuthor.Size = new System.Drawing.Size(300, 215);
+            this.dataGridViewAuthor.TabIndex = 2;
+            this.dataGridViewAuthor.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewAuthor_CellContentClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(713, 450);
+            this.Controls.Add(this.dataGridViewAuthor);
+            this.Controls.Add(this.btFind);
+            this.Controls.Add(this.tbFind);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewAuthor)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbFind;
+        private System.Windows.Forms.Button btFind;
+        private System.Windows.Forms.DataGridView dataGridViewAuthor;
+    }
+}
+
---
+++ LibraryWinFormEntity/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LibraryWinFormEntity
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            //var columnAuthorName = new DataGridViewColumn();
+            //columnAuthorName.CellTemplate = new DataGridViewTextBoxCell();
+
+            //var columnAuthorLastName = new DataGridViewColumn();
+            //columnAuthorLastName.CellTemplate = new DataGridViewTextBoxCell();
+
+            //var columnBookTitle = new DataGridViewColumn();
+            //columnBookTitle.CellTemplate = new DataGridViewTextBoxCell();
+
+            //dataGridViewAuthor.Columns.Add(columnAuthorName);
+            //dataGridViewAuthor.Columns.Add(columnAuthorLastName);
+            //dataGridViewAuthor.Columns.Add(columnBookTitle);
+
+        }
+
+        private void btFind_Click(object sender, EventArgs e)
+        {
+            if(dataGridViewAuthor==null)
+            dataGridViewAuthor.Rows.Clear();
+
+            using (Library3Entities db = new Library3Entities())
+            {
+
+                var authors = (from a in db.Author where a.LastName.Contains(tbFind.Text) select new { a.FirstName, a.LastName });
+
+                if(authors!=null)
+                dataGridViewAuthor.DataSource = authors.ToList();
+            }
+        }
+
+        private void tbFind_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridViewAuthor_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
