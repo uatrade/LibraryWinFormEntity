a3fba8cc2be2d4f51d952ce09cceb88cb24a0397
uatrade <piskovtsev@gmail.com>
2018-08-30 18:57:57 +0300

    Design

--- LibraryWinFormEntity/Form1.Designer.cs
+++ LibraryWinFormEntity/Form1.Designer.cs
@@ -31,12 +31,13 @@
             this.tbFind = new System.Windows.Forms.TextBox();
             this.btFind = new System.Windows.Forms.Button();
             this.dataGridViewAuthor = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewAuthor)).BeginInit();
             this.SuspendLayout();
             // 
             // tbFind
             // 
-            this.tbFind.Location = new System.Drawing.Point(103, 46);
+            this.tbFind.Location = new System.Drawing.Point(103, 71);
             this.tbFind.Name = "tbFind";
             this.tbFind.Size = new System.Drawing.Size(266, 20);
             this.tbFind.TabIndex = 0;
@@ -44,11 +45,11 @@
             // 
             // btFind
             // 
-            this.btFind.Location = new System.Drawing.Point(103, 175);
+            this.btFind.Location = new System.Drawing.Point(103, 157);
             this.btFind.Name = "btFind";
-            this.btFind.Size = new System.Drawing.Size(253, 23);
+            this.btFind.Size = new System.Drawing.Size(266, 23);
             this.btFind.TabIndex = 1;
-            this.btFind.Text = "Find";
+            this.btFind.Text = "Find Books";
             this.btFind.UseVisualStyleBackColor = true;
             this.btFind.Click += new System.EventHandler(this.btFind_Click);
             // 
@@ -61,11 +62,21 @@
             this.dataGridViewAuthor.TabIndex = 2;
             this.dataGridViewAuthor.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewAuthor_CellContentClick);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(100, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Search by author";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(713, 450);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.dataGridViewAuthor);
             this.Controls.Add(this.btFind);
             this.Controls.Add(this.tbFind);
@@ -82,6 +93,7 @@
         private System.Windows.Forms.TextBox tbFind;
         private System.Windows.Forms.Button btFind;
         private System.Windows.Forms.DataGridView dataGridViewAuthor;
+        private System.Windows.Forms.Label label1;
     }
 }
 
